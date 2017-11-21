df03d9a8dfa442353b0b7eaf0573bf61476b423c
Paul Michalik <paul.michalik@outlook.com>
2017-11-21 15:21:07 +0100

    Added more sophisticated detection of vcpkg installation. Supports ~/.vcpkg location.

--- bootstrap_windows.bat
+++ bootstrap_windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "OrbSlamPlatform=x86"
+set "OrbSlamPlatform=x64"
 set "OrbSlamToolset=v141"
 set "OrbSlamBuildType=Debug"
 
@@ -8,20 +8,26 @@ if NOT "%~1"=="" set "OrbSlamPlatform=%~1"
 if NOT "%~2"=="" set "OrbSlamToolset=%~2"
 if NOT "%~3"=="" set "OrbSlamBuildType=%~3" 
 
-set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
+rem Locate vcpkg:
 if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_ROOT_DIR%"
+if not exist "%VcPkgDir%" set "VcPkgDir=%USERPROFILE%\.vcpkg\vcpkg"
+if not exist "%VcPkgDir%" set "VcPkgDir=%USERPROFILE%\Software\vcpkg\vcpkg"
+if not exist "%VcPkgDir%" set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
+
+if not exist "%VcPkgDir%" echo vcpkg not found in your system, bailing out & exit /b 1
+
 set "VcPkgTriplet=%OrbSlamPlatform%-windows-%OrbSlamToolset%"
 if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcpkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
 set "VcPkgPath=%VcPkgDir%\vcpkg.exe"
 
 echo. & echo Bootstrapping dependencies for triplet: %VcPkgTriplet% & echo.
 
-if not exist "%VcPkgDir%" echo vcpkg path is not set correctly, bailing out & exit /b 1
-
 rem ==============================
 rem Update and Install packages.
 rem ==============================
+call "%VcPkgDir%\bootstrap-vcpkg.bat"
 call "%VcPkgPath%" update
+call "%VcPkgPath%" remove --outdated --recurse
 call "%VcPkgPath%" install eigen3 opencv pangolin --triplet %VcPkgTriplet%
 
 set "VcPkgTripletDir=%VcPkgDir%\installed\%VcPkgTriplet%"
