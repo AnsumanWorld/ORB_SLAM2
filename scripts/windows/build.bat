ba6402c26d715a4a19ed7fa935e9eb405303ef30
PMichalik <paul.michalik@harman.com>
2018-01-11 11:49:27 +0100

    Split build.bat into 2. phases: configure.bat and build.bat. configure.bat only exports variables required for build.bat.

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -2,37 +2,12 @@
 
 setlocal
 
-set "OrbSlamPlatform=x86"
-set "OrbSlamToolset=v141"
-set "OrbSlamBuildType=Debug"
+call "%~dp0configure.bat" %*
+if errorlevel 1 echo configure error, bailing out & exit /b 1
 
-if NOT "%~1"=="" set "OrbSlamPlatform=%~1"
-if NOT "%~2"=="" set "OrbSlamToolset=%~2"
-if NOT "%~3"=="" set "OrbSlamBuildType=%~3" 
+echo OrbSlamBuildDir = %OrbSlamBuildDir%
+echo OrbSlamBuildType= %OrbSlamBuildType%
 
-set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
-set "VcPkgTriplet=%OrbSlamPlatform%-windows-%OrbSlamToolset%"
-set "VcPkgTripletDir=%VcPkgDir%\installed\%VcPkgTriplet%"
-if not exist "%VcPkgTripletDir%" echo %VcPkgTripletDir% does not exist, bailing out & exit /b 1
-set "CMAKE_PREFIX_PATH=%VcPkgTripletDir%;%CMAKE_PREFIX_PATH%"
-
-set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015"
-
-if "%OrbSlamPlatform%"=="x86" (
-    if "%OrbSlamToolset%"=="v140" set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015"
-    if "%OrbSlamToolset%"=="v141" set "OrbSlamCMakeGeneratorName=Visual Studio 15 2017"
-)
-
-if "%OrbSlamPlatform%"=="x64" (
-    if "%OrbSlamToolset%"=="v140" set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015 Win64"
-    if "%OrbSlamToolset%"=="v141" set "OrbSlamCMakeGeneratorName=Visual Studio 15 2017 Win64"
-)
-
-set "OrbSlamBuildDir=%~dp0..\..\products\cmake.msbuild.windows.%OrbSlamPlatform%.%OrbSlamToolset%"
-if not exist "%OrbSlamBuildDir%" mkdir "%OrbSlamBuildDir%"
-cd "%OrbSlamBuildDir%"
-
-call cmake.exe -G "%OrbSlamCMakeGeneratorName%" -DBUILD_EXAMPLES=ON "%~dp0..\.."
-call cmake.exe --build . --config %OrbSlamBuildType%
+call cmake.exe --build "%OrbSlamBuildDir%" --config %OrbSlamBuildType%
 
 endlocal
