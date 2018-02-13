db2278efb5d7d60fda680462fa0280961017da1a
PMichalik <paul.michalik@harman.com>
2018-02-13 18:09:03 +0100

    Update infrastructure scripts

--- scripts/windows/bootstrap.bat
+++ scripts/windows/bootstrap.bat
@@ -1,20 +1,20 @@
 @echo off
 
-set "OrbSlamPlatform=x64"
-set "OrbSlamToolset=v141"
-set "OrbSlamBuildType=Release"
+set "Platform=x64"
+set "Toolset=v141"
+set "BuildType=Release"
 
-if NOT "%~1"=="" set "OrbSlamPlatform=%~1"
-if NOT "%~2"=="" set "OrbSlamToolset=%~2"
-if NOT "%~3"=="" set "OrbSlamBuildType=%~3" 
+if NOT "%~1"=="" set "Platform=%~1"
+if NOT "%~2"=="" set "Toolset=%~2"
+if NOT "%~3"=="" set "BuildType=%~3"
 
 rem ----------------------------------
 rem Locate vcpkg using environment variables
 rem ----------------------------------
 set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
-set "VcpkgTriplet=%OrbSlamPlatform%-windows"
+set "VcPkgTriplet=%Platform%-windows"
 if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_ROOT_DIR%"
-if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcpkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
+if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcPkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
 
 rem ----------------------------------
 rem Try to look for vcpkg at default locations
@@ -28,43 +28,30 @@ if not exist "%VcPkgDir%" (
     echo vcpkg not found, installing at %VcPkgDir%...
     git clone --recursive https://github.com/Microsoft/vcpkg.git "%VcPkgDir%"
     call "%VcPkgDir%\bootstrap-vcpkg.bat"
-) else (
-    echo vcpkg found at %VcPkgDir%...
+) 
+    
+echo vcpkg at %VcPkgDir%...
 
-    rem
-    rem Check whether we have a difference in the toolsrc folder. If non empty, %errorlevel% should be 0  
-    rem git diff --name-only origin/HEAD remotes/origin/HEAD | find "toolsrc/" > NUL & echo %errorlevel%
-    rem Put this to local function or better script...
-    rem
+rem
+rem Check whether we have a difference in the toolsrc folder. If non empty, %errorlevel% should be 0  
+rem git diff --name-only origin/HEAD remotes/origin/HEAD | find "toolsrc/" > NUL & echo %errorlevel%
+rem Put this to local function or better script...
+rem
 
-    pushd "%VcPkgDir%"
-    git pull --all --prune
-    popd
+pushd "%VcPkgDir%"
+git fetch origin 51e8b5da7cd8fd1273a99dac953de1aa193e7ac9
+git checkout FETCH_HEAD
+rem git pull --all --prune
+popd
 
-    rem
-    rem only invoke when changes to "toolsrc/" were made
-    rem 
-    rem call "%VcPkgDir%\bootstrap-vcpkg.bat"
-)
-
-if not exist "%VcPkgDir%" echo vcpkg path is not set correctly, bailing out & exit /b 1
-
-echo. & echo Bootstrapping dependencies for triplet: %VcPkgTriplet% & echo.
-
-set "VcPkgPath=%VcPkgDir%\vcpkg.exe"
-
-if not exist "%VcPkgPath%" echo vcpkg path is not set correctly, bailing out & exit /b 1
+rem
+rem only invoke when changes to "toolsrc/" were made
+rem 
+rem call "%VcPkgDir%\bootstrap-vcpkg.bat"
 
 rem ==============================
 rem Upgrade and Install packages.
 rem ==============================
-call "%VcPkgPath%" upgrade --no-dry-run --triplet %VcPkgTriplet%
-call "%VcPkgPath%" install boost-filesystem eigen3 opencv pangolin --triplet %VcPkgTriplet%
-
-set "VcPkgTripletDir=%VcPkgDir%\installed\%VcPkgTriplet%"
-
-if not exist "%VcPkgTripletDir%" echo %VcPkgTripletDir% does not exist, bailing out & exit /b 1
-
-set "CMAKE_PREFIX_PATH=%VcPkgTripletDir%;%CMAKE_PREFIX_PATH%"
-
-echo. & echo Bootstrapping successful for triplet: %VcPkgTriplet% & echo CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH% & echo.
+set "VcPkgDeps=boost-filesystem eigen3 opencv pangolin"
+call "%VcPkgDir%\vcpkg.exe" upgrade %VcPkgDeps% --no-dry-run --triplet %VcPkgTriplet%
+call "%VcPkgDir%\vcpkg.exe" install %VcPkgDeps% --triplet %VcPkgTriplet%
