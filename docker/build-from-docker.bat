64b59f25c86f026e22b313071d777f1d655913de
Paul Michalik <paul.michalik@outlook.com>
2018-05-04 15:07:12 +0200

    Synchronize build-from-docker scripts

--- docker/build-from-docker.bat
+++ docker/build-from-docker.bat
@@ -2,5 +2,8 @@
 
 setlocal
 call "%~dp0abspath.bat" "%~dp0.."
-docker run --rm -t --name orb-slam2-build --volume="%AbsPath%:/root/orb-slam2" shanmukhananda/orb-slam2:latest /bin/bash -c "/root/orb-slam2/scripts/linux/build.sh"
+docker run --rm -it --name orb-slam2-build --volume="%AbsPath%:/root/orb-slam2" ^
+    shanmukhananda/orb-slam2:latest ^
+    /bin/bash -c ^
+    "/root/orb-slam2/scripts/linux/build.sh"
 endlocal
--- docker/build-from-docker.sh
+++ docker/build-from-docker.sh
@@ -6,7 +6,7 @@ ScriptDir=$(realpath $(dirname $0))
 ProjectDir=$(realpath ${ScriptDir}/..)
 cd ${ProjectDir}
 
-docker run --rm -it --volume="$(pwd):/root/orb-slam2" \
+docker run --rm -it --name orb-slam2-build --volume="$(pwd):/root/orb-slam2" \
     shanmukhananda/orb-slam2:latest \
     /bin/bash -c \
     "/root/orb-slam2/scripts/linux/build.sh"
